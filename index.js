c7613be83f884f5f2b1befa7dafda2c273cf51ae
sourav <souravmahato6563@gmail.com>
2026-06-28 00:06:22 +0530

    feat: initialize project with basic JavaScript logic examples and HTML structure

---
+++ index.js
@@ -0,0 +1,55 @@
+
+function display() {
+  var  honey = 'nayan mahato'
+    console.log(honey);
+    
+}
+display() 
+
+const honey = 10
+const sunny = "11"
+console.log(sunny+(honey+15)); // here i should remeber to use its not concate we can use ()  for high preiority
+//when one number and one string plus its create string and its not adition or plus its string co
+
+//hey listen just remember if use - or any other math related symbol than actually concat will not work 
+
+
+// console.log("5"/2);
+
+// let age = Number(prompt("What is Your Age"))
+// console.log(age)
+
+/*hey listen just remember when we use like convert string to number number to string  or any data type to a diff type we use 
+that called type casting or type conversion  rember that
+ex- */
+
+// console.log(Number("50"))
+
+//lets do swap
+let a = 10 
+let b = 30
+let c = a
+a= b
+b = c
+
+
+ console.log(a);
+ console.log(b)
+
+
+ let m = 22;
+ let n = 33;
+ [m,n]=[n,m]
+ console.log(m);
+ console.log(n);
+
+
+
+ let i = 5
+ let j = 10
+ console.log(i-j); 
+
+ console.log(10%5);
+ 
+ 
+ 
